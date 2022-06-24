65c58e84e42498aa7ccc1358a5af43f36c11ca62
Valeriy Putilin <putilinvalera2004@gmail.com>
2022-06-24 13:16:51 +0300

    Add person1 Vlad

--- src/main.java
+++ src/main.java
@@ -3,5 +3,6 @@ public class main {
         Person person = new Person("Valeriy", 18);
         System.out.println("Hello world");
         person.SayHi();
+        Person person1 = new Person("Vlad", 18);
     }
 }
